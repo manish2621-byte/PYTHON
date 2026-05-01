9d1d9f2f6648e787a2bf37acee2b534d45d850c1
mod <manishsharma262103@gmail.com>
2026-05-01 12:04:59 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,34 @@
+#print("hello world")
+# print("hello world")
+#print("python","java","phy","android",sep=" ",end=" | ")
+#print(r"he\tll\bo \" \n python")
+#name = "manish"
+#email = "manish@example.com"
+#print("my name is {1} and email is {0}".format(email,name))
+#print("my name is manish", "   ", "my age is 23")
+#print(35+26)
+#name="manu"
+#email="manu@example.com"
+#print("my name is {0} and email is {1}")
+"""name ="manish"
+age = 23
+price =25.44
+print(name, age, price)
+print(type(name),type(age),type(price))
+a=2
+b=5
+sum= a+b
+print("sum of a nd b is",sum)
+diff = b - a
+print(diff)
+S,Y=2,3
+Txt="@"
+print(2*S*Y)
+a,b=5,-10
+print(a%b)
+input("enter yr name")
+print("name is",name)
+age= int(input("enter yr age : "))
+print(" age is ",age)
+print(2**5)"""
+print("hellowor\bld")
