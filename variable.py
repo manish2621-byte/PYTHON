9129394355517f3c1bc1bda83dc56480507baaf0
mod <manishsharma262103@gmail.com>
2026-05-04 09:43:02 +0530

    4may

---
+++ variable.py
@@ -0,0 +1,24 @@
+from unicodedata import numeric
+
+#int , float , string , list , tuple , set , dict , boolean
+numeric
+ # id=10
+#print(type(id))
+# price = 45.69
+# print(type(price))
+# name = "manish"
+# print(type(name))
+#subject =[ "python","java","phy","android"]
+#print(type(subject))
+# lang=("gujrati","english","hindi")
+#print(type(lang))
+
+#map
+person = {"name":"manish","age":23,"email":"manish@example.com"}
+print(type(person))
+#Boolean
+is_valid = True
+print(type(is_valid))
+a=int(input("enter a number : "))
+print(type(a))
+
