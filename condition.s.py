0e86745b830122723192a2d77b9d75241539347b
mod <manishsharma262103@gmail.com>
2026-05-04 16:03:08 +0530

    changed

---
+++ condition.s.py
@@ -0,0 +1,24 @@
+# light = input("light :")
+# if(light == "red"):
+#          print("stop")
+# elif(light == "yellow"):
+#          print("ready")   
+
+# elif(light == "green"):
+#          print("go")
+# else:
+#          print("invalid light")
+# A=5
+# G="M"
+# B=2
+# G="F"
+# single line if/ ternary operator
+
+# food = input("food : ")
+# eat = "yes" if food == "paneer" else "no"
+# print(eat)
+# food =input("food : ")
+# print ("protein") if food == "oats" else print("carbs") if food == "bread" else print("unknown food")
+#clever ternary operator
+age= int(input("age : "))
+vote=("yes" , " no")[age >= 18] 
