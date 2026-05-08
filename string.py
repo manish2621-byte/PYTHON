590bc822980b855c8fd73051d46bbd4114662d3b
mod <manishsharma262103@gmail.com>
2026-05-08 15:23:58 +0530

    may8

--- string.py
+++ string.py
@@ -1,11 +1,43 @@
-#TYPE OF STRING
-str1="manish"
-str2='manish'
-str3="""manish"""
-st = "sun rises in east"
+# #TYPE OF STRING
+# str1="manish"
+# str2='manish'
+# str3="""manish"""
+# st = "sun rises in east"
 # print(len(st))
 # print(st.lower())
 # print(st.casefold())
-print(st.upper())
-print(st.title())
-print(st.capitalize())
+# print(st.upper())
+# print(st.title())
+# # print(st.capitalize())
+# str ="manish" # -6,-5,-4,-3,-2,-1
+# print(str[0: ])
+# print(str[0:3])
+# print(str[-5:-1])
+# print(str[-3:-1])
+# #str.endswith("last character")
+# print(str.endswith("app"))
+ 
+# #str.capticalize()
+  
+# print(str.capitalize())
+# print(str)
+# str = str.capitalize()
+# print(str)
+# #str.replace("old","new")
+
+# str = str.replace("Manish","Manish sharma")
+# print(str)
+#
+# str1 = str1.replace("a","e")
+# print(str1)
+# print(str1.count("e"))
+
+# #str.find("substring")
+# print(str1.find("q"))
+#str.count("xyz")
+#str.startswith("first character")
+str="manish sharma"
+print(str.startswith("m"))
+str1=" hii i am the $ symbol $$$"
+print(str1.count("$"))
+
