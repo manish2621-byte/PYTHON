daf2856d197cb953078a8b8c12f836ec03cdde8f
mod <manishsharma262103@gmail.com>
2026-05-19 14:11:34 +0530

    changed

--- ASSIGNMENT/MODULE-6/lab_2.py
+++ ASSIGNMENT/MODULE-6/lab_2.py
@@ -1,5 +1,6 @@
 """Write a Python program that demonstrates the correct use of indentation, comments, and
 variablesfollowing PEP 8 guidelines."""
+
 #calculate the persentage  of student
 student_name="manish sharma"
 science_marks=80
