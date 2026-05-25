3179f9048daf607fc5e7507659195f84e1726dd7
mod <manishsharma262103@gmail.com>
2026-05-25 11:42:51 +0530

    changed

---
+++ CLASSWORK/function.py
@@ -0,0 +1,52 @@
+# def test():
+#     print("test calling")
+# def square(a):
+#     print(a*a)
+
+# def add(a,b):
+#     print(a+b)
+
+# def message():
+#     return "hello"
+
+# def cude(a):
+#     return a*a*a
+
+# test()
+# square(10)
+# square(20)
+# add(10,20)
+# t = message()
+# print(t)
+# print(message())
+
+# print(cube(3))
+
+# def person(name,email,age):
+#     print(name,email,age)
+
+# person("manish","manishsharma@gmail.com","age=23")
+
+# person("abc","abc@gmail.com",age=25)
+# def sum(*a):
+#     sum=0
+#     for i in a:
+#         sum+=i
+#     print(sum)
+#     sum(10,20,30,50)
+
+# def student(**a):
+#     print(a)
+
+# student(name="manish",email="manish@",age=23)
+# add = lambda a,b:a+b
+# sq = lambda a:a*a
+a=10
+# def test():
+#     global a
+#     a = 20 
+#     print(a)
+
+# print(a)
+# test()
+# print(a)   
--- CLASSWORK/practices.py
+++ CLASSWORK/practices.py
@@ -1,98 +1,70 @@
-# # question 1
-# print("hello manish")
-# # Q2
-# name="manish"
-# age=23
-# print(f"my name is {name} and my age is {age}")
-# CALCULATOR 
-# a=55
-# b=20
-# print("operator is +,-,/,//,%,**")
-# operator=input("enter your operator : ")
-# if(operator == '+'):
-#     print("addition is ",a+b)
-# elif(operator == '-'):
-#     print("substraction is ",a-b)
-# elif(operator == '/'):
-#     print("divison is ",a/b)
-# elif(operator == '//'):
-#     print("floor divison is ",a//b)
-# elif(operator == '%'):
-#     print("modulos is ",a%b)
-# elif(operator == '**'):
-#     print("exponentitation ",a**b)
+# # list append
+# fruits=["apple","mango","watermelon"]
+# fruits.append("banana")
+# print(fruits)
 
-'''Q3'''
-# name="manish"
-# age=23
-# persent=80
-# print(name)
-# print(age)
-# print(persent)
-# name=input("enter your name ")
-# age= int(input("enter your age "))
-# print(type(name))
-# print(type(age))
-'''Q4'''
-# a=55
-# b=55
-# if(a>b):
-#     print("greater than b ")
-# elif(a==b):
-#     print("a and b equal ")
-# else:
-#     print("b is greater than a")
-# a=2
-# if(a%2!=0):
-#     print("is a prime number")
-# else:
-#     print("is not prime number")
-# # caculate the grade using if else
-# persentage=int(input("enter your persentage "))
-# if(persentage >= 91 ):
-#     print("GRADE A")
-# elif(persentage >= 90 and persentage <= 80):
-#     print("GRADE B")
-# elif(persentage >= 79 and persentage <= 60):
-#     print("GRADE C")
-# elif(persentage >= 59 and persentage <= 35):
-#     print("GRADE D")
-# elif(persentage <= 33):
-#     print("FAIL")
-# else:
-#     print("invalid persentage : ")
+# #clear
+# fruits=["apple","mango","watermelon"]
+# fruits.clear()
+# print(fruits)
 
-#eligiable to blood donate
-# age=int(input("enter your age : "))
-# weight=float(input("enter your weight :"))
-# health=input(" are you healthy?  (yes/no) :" )
-# if(age >=18):
-#     print("you are eligiable for blood donate")
-#     if(weight>=50):
-#          print(" you are eligiable for blood donate")
-#          if(health == "yes"):
-#               print(" you are eligiable for blood donate")
-#          else:
-#               print("you are not healthy for blood donate")
-#     else:
-#          print("your weight below to 50kg that why you not donate blood")
-# else:
-#      print("your age is less than 18 ")         
-# str=" MANISH SHARMA "
-# #print(str.lower())
-# # print(str.capitalize())
-# # print(str.title())\
-# # print(str.strip())
-# # append()
-# a=[5,10,20]
-# # a.append(4)
-# # a.extend([1,2,3])
-# # a.remove(1)
-# print(a.index(10))
-          
+# #copy
+# fruits=["apple","mango","watermelon"]
+# x=fruits.copy()
+# print(x)
+
+# #extend
+# car=["porche","BMW","audi","volvo","merchedes"]
+# fruits=["apple","mango","watermelon"]
+# car.extend(fruits)
+# print(car)
 
 
+# #index
+# fruits=["apple","mango","watermelon"]
+# a=fruits.index("mango")
+# print(a)
     
+# #insert
+# fruits=["apple","mango","watermelon"]
+# fruits.insert(0,"banaana")
+# print(fruits)
 
+# #reverse
+# fruits=["apple","mango","watermelon"]
+# fruits.reverse()
+# print(fruits)
+
+# cars = ['Ford', 'BMW', 'Volvo']
+
+# cars.sort()
+
+# print(cars)
+
+#loop list
+
+# fruits=["apple","mango","watermelon"]
+# for a in fruits:
+#     print(a)
+
+# # len and range
+
+# fruits=["apple","mango","watermelon"]
+# # for a in range(len(fruits)):
+# #     print(a)
+# i=0
+# while i < len(fruits):
+#     print(fruits[i])
+#     i=+1
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+#list comprehension
+thislist = ["apple", "banana", "cherry"]
+[print(x) for  x in thislist]
 
 
