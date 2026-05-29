b3465b50c3e2e2a125a813bd76f62c586a88a877
mod <manishsharma262103@gmail.com>
2026-05-29 16:30:31 +0530

    changed

---
+++ CLASSWORK/myapp.py
@@ -0,0 +1,43 @@
+# import mod as c
+# import test as t
+
+# c.add(10,20)
+# c.square(10)
+# t.add(100,200)
+
+# from test import *
+# from mod import *
+# add(1000,2000)
+# square(1000)
+
+
+import math
+print(math.sqrt(100))
+print(math.pow(2,2))
+print(math.pi)
+print(math.floor(45.999))
+print(math.ceil(45.001))
+print(round(45.5678,2))
+print(math.e)
+print(math.log(10))
+
+
+import os
+os.mkdir("myfolder")
+os.rmdir("myfolder")
+
+import sys
+print(sys.version)
+
+import random
+print(random.randint(1,100))
+
+
+import datetime
+print(datetime.datetime.now())
+
+import calendar
+print(calendar.month(2024,6))
+
+
+
---
+++ HOMEWORK/practics.py
@@ -0,0 +1,84 @@
+
+# def name():
+#     print("hello manish")
+
+# def addition():
+#     a=input("enter the first number: ")
+#     b=input("enter the second number: ")
+#     print("the sum is",int(a)+int(b))
+
+# def substraction():
+#     a=input("enter the first number: ")
+#     b=input("enter the second number: ")
+#     print("the difference is",int(a)-int(b))
+
+# def multipication():
+#     a=int(input("enter the first number: "))
+#     b=int(input("enter the second number: "))
+#     print("the difference is",a*b)
+
+# def division():
+#     a=int(input("enter the first number: "))
+#     b=int(input("enter the second number: "))
+#     print("the difference is",a/b)
+
+# def modulos():
+#     a=int(input("enter the first number: "))
+#     b=int(input("enter the second number: "))
+#     print("the difference is",a%b) 
+
+
+    
+    
+
+# name()
+# addition()
+# substraction()
+# multipication()
+# division()
+# modulos()
+
+
+
+#calculator using function with return type
+
+
+# def addition(a,b):
+#     return a+b
+
+# def substraction(a,b):
+#     return a-b
+
+# def multipication(a,b):
+#     return a*b
+
+# def division(a,b):
+#     return a/b
+
+# def modulos(a,b):
+#     return a%b
+
+
+# operators=input("enter the operation you want to perform: ")
+# a=int(input("enter the first number: "))
+# b=int(input("enter the second number: "))
+
+# match(operators):
+#     case "+":
+#         print("the sum is",addition(a,b))
+#     case "-":
+#         print("the difference is",substraction(a,b))
+#     case "*":
+#         print("the product is",multipication(a,b))
+#     case "/":
+#         print("the quotient is",division(a,b))
+#     case "%":
+#         print("the remainder is",modulos(a,b))
+
+
+
+# addition()
+# substraction()
+# multipication()
+# division()
+# modulos()
