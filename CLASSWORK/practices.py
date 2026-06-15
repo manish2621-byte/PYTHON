d67faf6790a669e36578f1009db20341f39744c9
mod <manishsharma262103@gmail.com>
2026-06-15 17:23:47 +0530

    15/6/2026

--- CLASSWORK/practices.py
+++ CLASSWORK/practices.py
@@ -4,10 +4,76 @@
 #         l.append(str(i))
 # print(','.join(l))
 
-def fact(n):
-    if n==0:
-        return 1
-    else:
-        return n*fact(n-1)
-n=int(input("enter a number: "))
-print(fact(n))
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+# n=int(input("enter a number: "))
+# print(fact(n))
+ 
+
+# def calc_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum(1, 2)
+
+# calc_sum(12, 5)
+
+# calc_sum(45, 78)
+
+# def aver_3(a, b, c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# aver_3(98, 97, 95)
+
+cities = ["delhi", "surat","mumbai","bangluru","chennai"]
+heroes = ["thor", "ironman","captain america","shaktiman"]
+
+# def items(list):
+#     for item in list:
+#         print(item,end=" ")
+
+# items(cities)
+
+# items(heroes)
+
+# def fact(x):
+#     if x == 0:
+#         return 1
+#     return x * fact(x - 1)
+
+# x=int(input())
+# print(fact(x))
+
+# n=int(input())
+# d=dict()
+# for i in range(1,n+1):
+#     d[i]=i*i
+
+# print(d)
+
+# values =input()
+# l=values.split(",")
+# t=tuple(l)
+# print(l)
+# print(t)
+
+
+class inputoutstring(object):
+    def __init__(self):
+        self.s =""
+    def getstring(self):
+        self.s = input()
+    def printstring(self):
+        print(self.s.upper())
+
+    
+strObj = inputoutstring()
+strObj.getstring()
+strObj.printstring()
